32f5a23101c370447bee5b207b2fefdebda90e2e
Miljan Milidrag <m.milidrag25@gmail.com>
2018-09-26 08:55:39 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          number/numer
+          headline
+        </div>
+        <div>
+          Review
+        </div>
+        <div>
+          curasel
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
